15d7f700fb4cc10214341ac64a670a791fb183d0
pinky-pig <2545589993@qq.com>
2022-10-08 16:48:48 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: '@antfu/eslint-config',
+  rules: {
+    // 禁止出现 console
+    'no-console': 'off',
+    // 禁止出现 debugger
+    'no-debugger': 'off',
+    // @typescript-eslint禁止出现未使用过的变量
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.md', '**/*.md/*.*', 'demo.vue', 'scripts/*.ts', '*.test.ts'],
+      rules: {
+        'no-alert': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+}
