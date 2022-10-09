2f0f2ee0ce955ab771fd973da10ccbaca873c53e
pinky-pig <2545589993@qq.com>
2022-10-09 18:03:07 +0800

    chore: add blog - code formatter

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CardGroup: typeof import('./components/CardGroup.vue')['default']
     ConfettiCanvas: typeof import('./components/ConfettiCanvas.vue')['default']
+    copy: typeof import('./components/Group copy.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     Header: typeof import('./components/Header.vue')['default']
     HelloWorld: typeof import('./components/HelloWorld.vue')['default']
--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
-
+const list = [
+  {
+    title: 'Icons Basic Edit',
+    subtitle: 'To learn svg',
+    address: 'https://github.com/pinky-pig/icons-basic-edit.git',
+    imgUrl: 'https://cdn.jsdelivr.net/gh/pinky-pig/pic-bed/images20221002104728.png',
+    preview: 'https://pinky-pig.github.io/icons-basic-edit/',
+  },
+  {
+    title: 'What Is My Blog',
+    subtitle: 'To notes and share',
+    address: 'https://github.com/pinky-pig/what-is-my-blog.git',
+    imgUrl: 'https://cdn.jsdelivr.net/gh/pinky-pig/pic-bed/images20221002110434.png',
+    preview: 'https://pinky-pig.github.io/what-is-my-blog/',
+  },
+]
 </script>
 
 <template>
-  <br>
-  blog
+  <ul>
+    <router-link to="/post/code-formatter" class="item block font-normal mb-6 mt-2 no-underline cursor-pointer">
+      <div class=" flex flex-col items-start">
+        <!-- title -->
+        <div class="title text-lg leading-1.2em">
+          Git - Formatter Code
+        </div>
+        <!-- time -->
+        <div class="opacity-50 text-sm">
+          2022/10/09
+        </div>
+      </div>
+    </router-link>
+  </ul>
 </template>
 
 <style lang="less" scoped>
-
+a{
+  background-image:linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0))!important;
+}
 </style>
