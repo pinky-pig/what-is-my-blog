6b1e306e5fc66b9fda57877af701702fe26c0193
pinky-pig <2545589993@qq.com>
2022-10-19 16:38:50 +0800

    feat: card 夜间模式颜色

--- src/components/CustomCard.vue
+++ src/components/CustomCard.vue
@@ -6,7 +6,7 @@
 
 <style lang="less" scoped>
 .custom-card{
-  @apply bg-white relative w-full h-full;
+  @apply bg-white dark:bg-[#121212] relative w-full h-full;
   max-width: 80ch;
   border-radius: 0.5rem;
   padding: 1rem;
@@ -25,7 +25,7 @@
   top: 0.6em;
   left: 0.6em;
   transition: top .25s,left .25s;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4'%3E%3Cpath d='m0 0 4 4Zm4 0L0 4Z' stroke-width='.5' stroke='%23000'/%3E%3C/svg%3E");
+  background-image: var(--bg-border-svg);
   transition: top .25s,left .25s;
   content: "";
 }
