db915a845cf72559fea5829a21f8e62abd0f9796
pinky-pig <2545589993@qq.com>
2022-10-22 16:49:57 +0800

    feat:post适配移动端路由

--- src/views/post/index.vue
+++ src/views/post/index.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
 import '../article/index'
+import { modules } from '~/views/article'
+
 const router = useRouter()
 
 const isLargeScreen = useMediaQuery('(min-width: 1024px)')
-watchEffect(() => {
-  if (isLargeScreen.value) {
-    router.push({ path: '/post/0' })
-  }
-})
+if (isLargeScreen.value) {
+  router.push({ path: '/post/0' })
+}
 
+const reg = /\.\/(\S*)\.md/
+let list = Object.keys(modules).map((item) => {
+  const result = item.match(reg)
+  if (result)
+    return result[1]
+  else
+    return ''
+})
+// 删除假值 false 0 undefined null NaN
+list = list.filter(Boolean)
 </script>
 
 <template>
   <CustomCard>
-    Post
+    <div class="flex flex-col">
+      <div
+        v-for="(it, idx) in list"
+        :key="idx"
+        class="list"
+        @click="router.push({ path: `/post/${idx}` })"
+      >
+        第 {{ idx + 1 }} 期 - {{ it }}
+    </div>
+    </div>
   </CustomCard>
 </template>
 
